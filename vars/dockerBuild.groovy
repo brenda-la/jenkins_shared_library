b495b73b7117ac2232b4028bf2d85e2b110be431
brenda <brendadongo56@gmail.com>
2023-07-03 11:23:03 +0100

    dock

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,9 @@
- def call(){
+def call(String project, String ImageTag, String hubUser){
 
     sh """
-     docker image build -t ${hubUser}/${project} .
+     docker image build -t ${hubUser}/${project} . 
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
 """
  }
  
